3f5b4fc5e532a1834c6d03340ec411db63347c83
Denis <Denis3094@mail.ru>
2020-12-17 03:50:54 +0700

    Homework_2

---
+++ 4/app.js
@@ -0,0 +1,45 @@
+'use strict'
+/**
+ * Функция sum складывает параметры a и b;
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} Возвращает так же число, складывая данные параметры.
+ */
+
+function sum (a, b) {
+    return a + b;
+}
+
+/**
+ * Функция sub вычитает из параметра a параметр b;
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} Возвращает так же число, вычитая данные параметры.
+ */
+
+function sub (a, b) {
+    return a - b;
+}
+
+/**
+ * Функция multiplier перемножает параметры a и b;
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} Возвращает так же число, умножая данные параметры.
+ */
+
+function multiplier (a, b) {
+    return a * b;
+}
+
+/**
+ * Функция division делит параметр "a" на параметр "b";
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} Возвращает так же число, поделив данные параметры.
+ */
+
+function division (a, b) {
+    return a / b;
+}
+
